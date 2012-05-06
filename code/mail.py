4c20be453a5bd3191d9f962c5d4c060355b741ae
Federico Ceratto <federico.ceratto@gmail.com>
2012-05-06 11:02:10 +0100

    message expiration fixed, shadow support added

--- code/mail.py
+++ code/mail.py
@@ -15,7 +15,7 @@ __change = False
 
 MSG_MAX = 5
 MSG_MARGIN = 5
-MSG_EXPIRY_TIME = 20
+MSG_EXPIRY_TIME = 5
 
 def Has_New_Mail():
     global __messages, __change
@@ -37,6 +37,10 @@ def Has_New_Mail():
     return x
 
 def Draw_Mail(output):
+    # Purge old messages, one for each run
+    if __messages and __messages[0][0] < time.time():
+        __messages.pop(0)
+
     # Show current messages
     y = output.get_rect().height - MSG_MARGIN
 
@@ -52,11 +56,34 @@ def Set_Day(day):
     global __day
     __day = int(day)
 
-def New_Mail(text, colour=(128,128,128)):
+def blur_surf(surface, blur):
+    scale = 1.0 / float(blur)
+    surf_size = surface.get_size()
+    scale_size = (int(surf_size[0] * scale), int(surf_size[1] * scale))
+    surf = pygame.transform.smoothscale(surface, scale_size)
+    surf = pygame.transform.smoothscale(surf, surf_size)
+    return surf
+
+def pretty_text_render(text, colour, background=(0, 0, 0), fsize=20, blur=1.5):
+    """Render text with a blurred background color"""
+    s = stats.Get_Font(fsize).render(text, True, background)
+    s = blur_surf(s, blur)
+    for x in xrange(2): # strenghten the shadow
+        s.blit(s, (0, 0))
+    fg = stats.Get_Font(fsize).render(text, True, colour)
+    for x in xrange(2): # strenghten the shadow
+        s.blit(fg, (0, 0))
+    return s
+
+def New_Mail(text, colour=(255,255,255)):
     global __messages, __day, __change
     text = ( "Day %u: " % __day ) + text
-    __messages.append((time.time() + MSG_EXPIRY_TIME, 
-            stats.Get_Font(20).render(text, True, colour)))
+    s = pretty_text_render(text, colour)
+    __messages.append((time.time() + MSG_EXPIRY_TIME, s))
+
+    if len(__messages) > MSG_MAX:
+        __messages.pop(0)
+
     __change = True
 
 def Initialise():
